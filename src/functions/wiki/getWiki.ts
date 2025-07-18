8526ae62f0a4b8780a55b3bf1d061380a68d40e2
Zack <muhammadbinasim471@gmail.com>
2025-07-18 12:58:50 +0500

    feat: Create function getWiki

---
+++ src/functions/wiki/getWiki.ts
@@ -0,0 +1,30 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript"
+import { redditFetch } from "../../utils/redditFetch"
+import { ForgeSocial } from "../.."
+
+export default new NativeFunction({
+  name: "$getWiki",
+  version: "1.0.0",
+  description: "Get the subreddit wiki index page of the name you gave",
+  args: [
+    {
+      name: "subreddit",
+      description: "The subreddit to get the wiki index page of",
+      type: ArgType.String,
+      rest: true,
+      required: true,
+    }
+  ],
+  brackets: true,
+  unwrap: true,
+  output: ArgType.Json,
+
+  async execute(ctx, [subreddit]) {
+    const ext = ctx.client.getExtension("ForgeSocial") as ForgeSocial
+    const username = await ext?.getUsername()
+    if (!username) return this.customError("No Reddit username found at index file.")
+
+    let json = await redditFetch(`r/${subreddit}/wiki/index.json`, username)
+    return this.success(JSON.stringify(json, null, 2))
+  }
+})
