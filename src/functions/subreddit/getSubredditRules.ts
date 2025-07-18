4400754ca6bbed92974778461f158bdfc89bb730
Zack <muhammadbinasim471@gmail.com>
2025-07-18 12:12:13 +0500

    feat: create subreddit related functions

---
+++ src/functions/subreddit/getSubredditRules.ts
@@ -0,0 +1,29 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript"
+import { redditFetch } from "../../utils/redditFetch"
+import { ForgeSocial } from "../.."
+
+export default new NativeFunction({
+  name: "$getSubredditRules",
+  version: "1.0.0",
+  description: "Get the rules for a subreddit",
+  args: [
+    {
+      name: "subreddit",
+      description: "The subreddit to get the rules for",
+      type: ArgType.String,
+      rest: false,
+      required: true
+    }
+  ],
+  brackets: true,
+  unwrap: true,
+  output: ArgType.Json,
+  async execute(ctx, [subreddit]) {
+    const ext = ctx.client.getExtension("ForgeSocial") as ForgeSocial
+    const username = await ext?.getUsername()
+    if (!username) return this.customError("No Reddit username found.")
+    const json = await redditFetch(`r/${subreddit}/about/rules.json`, username)
+    if (!json.rules) return this.customError("No rules found for this subreddit.")
+    return this.success(JSON.stringify(json.rules, null, 2))
+  }
+})
