d58c846aff480cf5dbf9aefe900f307e97861301
Zack <muhammadbinasim471@gmail.com>
2025-07-20 11:34:37 +0500

    chore: Create youtube related enums

---
+++ src/utils/youtubeEnums.ts
@@ -0,0 +1,34 @@
+export enum UploadDate {
+  All = 'all',
+  Hour = 'hour',
+  Today = 'today',
+  Week = 'week',
+  Month = 'month',
+  Year = 'year',
+}
+
+export enum Duration {
+  All = 'all',
+  Short = 'short',
+  Medium = 'medium',
+  Long = 'long',
+}
+
+export enum SortBy {
+  Relevance = 'relevance',
+  Rating = 'rating',
+  UploadDate = 'upload_date',
+  ViewCount = 'view_count',
+}
+
+export enum Features {
+  HD = 'hd',
+  Subtitles = 'subtitles',
+  FourK = '4k',
+  Live = 'live',
+  ThreeSixty = '360',
+  ThreeD = '3d',
+  HDR = 'hdr',
+  CC = 'cc',
+  VR180 = 'vr180',
+}
