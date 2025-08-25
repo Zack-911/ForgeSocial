7818f0ed230adff0dc7f373d155d5c8894e39cab
Zack-911 <muhammadbinasim471@gmail.com>
2025-08-25 14:53:42 +0200

    feat: add githubListWorkflowJobs

---
+++ src/functions/github/actions/githubListWorkflowJobs.ts
@@ -0,0 +1,81 @@
+import { ArgType, NativeFunction } from '@tryforge/forgescript';
+import { ForgeSocial } from '../../..';
+import { handleGitHubError } from '../../../utils/errorHandler';
+
+enum filter {
+  latest = 'latest',
+  all = 'all',
+}
+
+export default new NativeFunction({
+  name: '$githubListWorkflowJobs',
+  description: 'Lists jobs for a workflow run',
+  brackets: true,
+  unwrap: true,
+  args: [
+    {
+      name: 'owner',
+      description: 'The owner of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'repo',
+      description: 'The name of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'runId',
+      description: 'The ID of the workflow run',
+      required: true,
+      rest: false,
+      type: ArgType.Number,
+    },
+    {
+      name: 'filter',
+      description: 'Filter jobs by status',
+      required: false,
+      rest: false,
+      type: ArgType.Enum,
+      enum: filter,
+    },
+    {
+      name: 'perPage',
+      description: 'Results per page (max 100)',
+      required: false,
+      rest: false,
+      type: ArgType.Number,
+    },
+    {
+      name: 'page',
+      description: 'Page number of the results to fetch',
+      required: false,
+      rest: false,
+      type: ArgType.Number,
+    },
+  ],
+  output: ArgType.Json,
+  async execute(ctx, [owner, repo, runId, filter, perPage, page]) {
+    const ext = ctx.client.getExtension('ForgeSocial') as ForgeSocial;
+    const github = ext.github;
+    if (!github) {
+      return this.customError('GitHub client not initialized');
+    }
+    try {
+      const jobs = await github.actions.listJobsForWorkflowRun({
+        owner,
+        repo,
+        run_id: runId,
+        filter: filter || undefined,
+        per_page: perPage || undefined,
+        page: page || undefined,
+      });
+      return this.success(JSON.stringify(jobs.data, undefined, 2));
+    } catch (e) {
+      return this.success(handleGitHubError(e));
+    }
+  },
+});
