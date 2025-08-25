755c17f860416f94f474b372aad57d0787c9396e
Zack-911 <muhammadbinasim471@gmail.com>
2025-08-25 14:53:42 +0200

    chore: add githubRemoveCollaborator

---
+++ src/functions/github/repositories/githubRemoveCollaborator.ts
@@ -0,0 +1,51 @@
+import { ArgType, NativeFunction } from '@tryforge/forgescript';
+import { ForgeSocial } from '../../../';
+import { handleGitHubError } from '../../../utils/errorHandler';
+
+export default new NativeFunction({
+  name: '$githubRemoveCollaborator',
+  description: 'Removes a collaborator from a repository',
+  brackets: true,
+  unwrap: true,
+  args: [
+    {
+      name: 'owner',
+      description: 'The owner of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'repo',
+      description: 'The name of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'username',
+      description: 'GitHub username of the collaborator',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+  ],
+  output: ArgType.Json,
+  async execute(ctx, [owner, repo, username]) {
+    const ext = ctx.client.getExtension('ForgeSocial') as ForgeSocial;
+    const github = ext.github;
+    if (!github) {
+      return this.customError('GitHub client not initialized');
+    }
+    try {
+      await github.rest.repos.removeCollaborator({
+        owner,
+        repo,
+        username,
+      });
+      return this.success(JSON.stringify({ success: true }, undefined, 2));
+    } catch (e) {
+      return this.success(handleGitHubError(e));
+    }
+  },
+});
