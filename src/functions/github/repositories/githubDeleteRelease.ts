3a8167989589d56fde3ffbd6df4cd07116c7e5bc
Zack-911 <muhammadbinasim471@gmail.com>
2025-08-25 14:53:42 +0200

    chore: add githubDeleteRelease

---
+++ src/functions/github/repositories/githubDeleteRelease.ts
@@ -0,0 +1,52 @@
+import { ArgType, NativeFunction } from '@tryforge/forgescript';
+import { ForgeSocial } from '../../../';
+import { handleGitHubError } from '../../../utils/errorHandler';
+
+export default new NativeFunction({
+  name: '$githubDeleteRelease',
+  description: 'Deletes a GitHub release',
+  brackets: true,
+  unwrap: true,
+  args: [
+    {
+      name: 'owner',
+      description: 'The owner of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'repo',
+      description: 'The name of the repository',
+      required: true,
+      rest: false,
+      type: ArgType.String,
+    },
+    {
+      name: 'releaseId',
+      description: 'The ID of the release to delete',
+      required: true,
+      rest: false,
+      type: ArgType.Number,
+    },
+  ],
+  output: ArgType.Boolean,
+  async execute(ctx, [owner, repo, releaseId]: [string, string, number]) {
+    const ext = ctx.client.getExtension('ForgeSocial') as ForgeSocial;
+    const github = ext.github;
+    if (!github) {
+      return this.customError('GitHub client not initialized');
+    }
+
+    try {
+      await github.rest.repos.deleteRelease({
+        owner,
+        repo,
+        release_id: releaseId,
+      });
+      return this.success(true);
+    } catch (e) {
+      return this.success(handleGitHubError(e));
+    }
+  },
+});
